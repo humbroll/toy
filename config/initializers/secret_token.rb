82d1a61a4f5747636abe9f6cb8592a3a17bb9bab
root <root@tcb1863.tcloud.infra>
2013-05-14 19:41:55 +0900

    init commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toy::Application.config.secret_token = '6348baf836053b26539913dbef5df605601aa920b934bc4ff36d77513e82731021c9218e5b63b3a55204162ad600d3895a3db48cd60a6004f9f5cb4e64240560'
